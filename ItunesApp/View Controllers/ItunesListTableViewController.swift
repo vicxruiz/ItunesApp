0821c782d1332d1627833cc0e7cf0be6cad2120f
victor <ruizdvictor@gmail.com>
2019-06-30 11:23:49 -0400

    implemented section header logic

--- ItunesApp/View Controllers/ItunesListTableViewController.swift
+++ ItunesApp/View Controllers/ItunesListTableViewController.swift
@@ -10,7 +10,39 @@ import Foundation
 import UIKit
 
 class ItunesListTableViewController: UITableViewController {
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+    }
+    
+    //MARK: SectionHeader
+    
+    override func tableView(_ tableView: UITableView, willDisplay cell: UITableViewCell, forRowAt indexPath: IndexPath) {
+        cell.backgroundColor = UIColor.white
+    }
+    
+    override func tableView(_ tableView: UITableView, willDisplayHeaderView view: UIView, forSection section: Int) {
+        //updates header view ui
+        view.tintColor = UIColor.white
+        guard let header = view as? UITableViewHeaderFooterView else {return}
+        
+        header.textLabel?.font = UIFont.boldSystemFont(ofSize: 24)
+        header.textLabel?.textColor = UIColor.lightGray
+        header.textLabel?.textAlignment = .left
+    }
+    
+    override func tableView(_ tableView: UITableView, heightForHeaderInSection section: Int) -> CGFloat {
+        //updates header height
+        return 44
+    }
+    
+    override func tableView(_ tableView: UITableView, titleForHeaderInSection section: Int) -> String? {
+        //logic for each section
+        if (section == 0) {
+            return "Songs"
+        } else {
+            return nil
+        }
     }
 }
