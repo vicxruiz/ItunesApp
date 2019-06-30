f79c9c6dca0d78591063a376dd7204a62dc544d1
victor <ruizdvictor@gmail.com>
2019-06-30 10:53:21 -0400

    constructed itunes song model

---
+++ ItunesApp/Models/Itunes.swift
@@ -0,0 +1,24 @@
+//
+//  Itunes.swift
+//  ItunesApp
+//
+//  Created by Victor  on 6/30/19.
+//  Copyright © 2019 Victor . All rights reserved.
+//
+
+import Foundation
+
+//structuring model objects to json format
+struct Root: Codable {
+    let feed: Feed
+}
+
+struct Feed: Codable {
+    let results: [Song]
+}
+
+struct Song: Codable {
+    let name: String
+    let artworkUrl100: String
+}
+
