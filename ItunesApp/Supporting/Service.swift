c3755bc595ce93415f941b0ffacedfc766d437a5
victor <ruizdvictor@gmail.com>
2019-06-30 11:07:53 -0400

    added class for ui and ux logic

---
+++ ItunesApp/Supporting/Service.swift
@@ -0,0 +1,21 @@
+//
+//  Service.swift
+//  ItunesApp
+//
+//  Created by Victor  on 6/30/19.
+//  Copyright © 2019 Victor . All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class Service {
+    static let buttonCornerRadius: CGFloat = 7
+    static func showAlert(on: UIViewController, style: UIAlertController.Style, title: String?, message: String?, actions: [UIAlertAction] = [ UIAlertAction(title: "OK", style: .default, handler: nil)], completion: (() -> Swift.Void)? = nil) {
+        let alert = UIAlertController(title: title, message: message, preferredStyle: style)
+        for action in actions {
+            alert.addAction(action)
+        }
+        on.present(alert, animated: true, completion: completion)
+    }
+}
