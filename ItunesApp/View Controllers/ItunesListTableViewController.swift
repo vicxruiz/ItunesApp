a4718fa48a2bcdb49191acb013bc6a5480eb206d
victor <ruizdvictor@gmail.com>
2019-06-30 11:21:40 -0400

    implemented storyboard logic and connected views to outlets

---
+++ ItunesApp/View Controllers/ItunesListTableViewController.swift
@@ -0,0 +1,16 @@
+//
+//  ItunesListTableViewController.swift
+//  ItunesApp
+//
+//  Created by Victor  on 6/30/19.
+//  Copyright © 2019 Victor . All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class ItunesListTableViewController: UITableViewController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
---
+++ ItunesApp/Views/ItunesMediaTableViewCell.swift
@@ -0,0 +1,15 @@
+//
+//  ItunesMediaTableViewCell.swift
+//  ItunesApp
+//
+//  Created by Victor  on 6/30/19.
+//  Copyright © 2019 Victor . All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class ItunesMediaTableViewCell: UITableViewCell {
+    @IBOutlet weak var mediaImageView: UIImageView!
+    @IBOutlet weak var mediaNameLabel: UILabel!
+}
